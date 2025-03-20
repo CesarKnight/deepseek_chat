c5c0ae64c73509af5cb247c7d686562817a4124a
Cesar Caballero <udemicscesar@gmail.com>
2025-03-19 23:50:03 -0400

    Respuesta basica con formato UTF8

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,5 +1,8 @@
 import 'package:deepseek_chat/consts.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/message.dart';
 
 import 'package:dash_chat_2/dash_chat_2.dart';
 
@@ -24,11 +27,11 @@ class _ChatPageState extends State<ChatPage> {
   );
 
   List<ChatMessage> _messages = <ChatMessage>[];
+  List<Message> _messagesHistory = <Message>[];
   
   // TODO: Add a list of messages to store the chat history
   // TODO: Implement a model for the messages
   // TODO: Implement a http client to request the chat response from the API
-  // List<Message> _messagesHistory = <Message>[];
 
   @override
   Widget build(BuildContext context) {
@@ -64,9 +67,49 @@ class _ChatPageState extends State<ChatPage> {
   Future<void> getChatResponse(ChatMessage m) async{
     setState(() {
       _messages.insert(0, m);
+      _messagesHistory.add(Message(
+        role: "user",
+        content: m.text,
+      ));
     });
 
-    // 
+    try {
+      final response = await http.post(
+        Uri.parse(API_URL),
+        headers: {
+          'Authorization': 'Bearer $OPENROUTER_API_KEY',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'model': OPENROUTER_MODEL,
+          'messages': _messagesHistory.map((msg) => msg.toJson()).toList(),
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(utf8.decode(response.bodyBytes));
+        final assistantMessage = data['choices'][0]['message']['content'];
+        
+        final botMessage = ChatMessage(
+          user: _deepseekUser,
+          text: assistantMessage,
+          createdAt: DateTime.now(),
+        );
+
+        setState(() {
+          _messages.insert(0, botMessage);
+          _messagesHistory.add(Message(
+            role: "assistant",
+            content: assistantMessage,
+          ));
+        });
+      } else {
+        print('Failed to get response: ${response.statusCode}');
+        print('Response body: ${response.body}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
 
   }
 }
